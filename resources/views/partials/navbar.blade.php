a8291e733f04289f14f5fa6eb9da845171b95050
Shivendra TechSter <shivendratechSter@gmail.com>
2020-04-25 13:13:35 +0530

    navbar search

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,47 +1,45 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="/">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-      </a>
-  
-      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
+        <a class="navbar-item" href="/">
+            <img src="/logo.svg" width="auto" height="28">
+        </a>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+           data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
     </div>
-  
+
     <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item {{ Request::is('posts') ? 'is-active' : '' }}" href="{{ route('posts.index') }}">
-          Posts
-        </a>
-        @can('manage-posts')
-        <a class="navbar-item {{ Request::is('posts/create') ? 'is-active' : '' }}" href="{{ route('posts.create') }}">
-          Create
-        </a>
-        @endcan
-      </div>
-  
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            @guest
-            <a class="button is-primary" href="/register">
-              <strong>Sign up</strong>
+        <div class="navbar-start">
+            <a class="navbar-item {{ Request::is('posts') ? 'is-active' : '' }}" href="{{ route('posts.index') }}">
+                Posts
             </a>
-            <a class="button is-light" href="/login">
-              Log in
-            </a>
-            @endguest
-            @auth
-            <form action="logout" method="POST">
-              @csrf
-              <button class="button is-light">Log out</button>
-            </form>
-            @endauth
-          </div>
+            @can('manage-posts')
+                <a class="navbar-item {{ Request::is('posts/create') ? 'is-active' : '' }}"
+                   href="{{ route('posts.create') }}">
+                    Create
+                </a>
+            @endcan
         </div>
-      </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="field has-addons">
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Find a post">
+                    </div>
+                    <div class="control">
+                        <a class="button is-info">
+                            <span class="material-icons">
+                                search
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </nav>
+</nav>
