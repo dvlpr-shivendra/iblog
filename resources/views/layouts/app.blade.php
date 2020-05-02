7c7c52ecce9344704d33cf06dc50f349b18cc474
Shivendra TechSter <shivendratechSter@gmail.com>
2020-05-02 22:09:42 +0530

    footer text change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
     <footer class="footer p-t-25 p-b-25">
         <div class="content has-text-centered">
           <p>
-            Designed & Developed by Shivendra TechSter with Laravel and Bulma CSS.
+            Designed & Developed by Shivendra TechSter
           </p>
         </div>
       </footer>
