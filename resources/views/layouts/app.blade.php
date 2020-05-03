17f901a6474b82823396736ba6623b4084904197
Shivendra TechSter <shivendratechSter@gmail.com>
2020-05-04 01:16:06 +0530

    footer extracted to partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,11 +53,7 @@
         </main>
     </div>
 
-    <footer class="footer p-t-25 p-b-25">
-      <div class="content has-text-centered">
-        <p>Designed & Developed by Shivendra TechSter</p>
-      </div>
-    </footer>
+    @include('partials.footer')
 
     <script src="{{ asset('js/app.js') }}"></script>
 
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer p-t-25 p-b-25">
+    <div class="content has-text-centered">
+      <p>Designed & Developed by Shivendra TechSter</p>
+    </div>
+</footer>
