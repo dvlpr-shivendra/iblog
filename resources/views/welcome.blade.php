8b29ae49b87c24384814a15c0d0ab67d05534c73
Shivendra <shivendratechster@gmail.com>
2020-08-10 10:36:17 +0530

    show featured post on landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,22 @@
 
 @section('content')
 
+@php $post = \App\Post::latest()->first() @endphp
+
 <section class="hero is-link is-fullheight-with-navbar">
   <div class="hero-body">
     <div class="container">
+      <p class="m-b-5">Featured post</p>
       <h1 class="title m-b-10 animated zoomIn">
-        Modest Developer
+        {{$post->title}}
       </h1>
-      <p class="animated fadeIn">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quae minima quos qui exercitationem error, labore tempora nobis earum numquam aut, dolores odio explicabo a illo ex ea placeat inventore ad possimus tempore? Mollitia totam dolores amet enim officia veniam accusantium dolore vel soluta aperiam!</p>
+      <p class="animated fadeIn is-size-5">{{$post->description}}</p>
       <a class="button is-light m-t-20 animated fadeIn" 
+        href="{{ $post->url() }}"
+      >Read Post</a>
+      <a class="button is-light is-outlined m-t-20 animated fadeIn" 
         href="{{ route('posts.index') }}"
-      >Browse posts</a>
+      >Browse Posts</a>
     </div>
   </div>
 </section>
